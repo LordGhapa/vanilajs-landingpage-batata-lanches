e572fae04c2284e1fed8113b5e1189b929cf6a1e
Lord Ghapa <felipef32@hotmail.com>
2023-06-06 20:51:34 -0300

    fix svg

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-import AOS from "aos";
-import "aos/dist/aos.css";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 // init AOS animation
 AOS.init({
-    duration: 1000,
-    offset: 100,
-});
+  duration: 1000,
+  offset: 100
+})
+
+const menuMobile = document.querySelector('.header__menu-mobile')
+const mobileShowIcon = document.querySelector('.header__menu-mobile-icon')
+const mobileLi = document.querySelectorAll('.header__menu-mobile a,.close-icon')
+
+function hiddenMobileMenu() {
+  menuMobile.classList.add('hidden')
+}
+function showMobileMenu() {
+  menuMobile.classList.remove('hidden')
+}
+
+mobileShowIcon.addEventListener('click', showMobileMenu)
+mobileLi.forEach(li => {
+    li.addEventListener('click', hiddenMobileMenu);
+  });
+
+
